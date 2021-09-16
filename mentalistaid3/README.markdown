e7a65c5d68230726f5a790e8de6f6644daa7a5a9
Heloísa Rades <83662938+hrades@users.noreply.github.com>
2021-09-16 17:34:42 -0300

    Aula 03 - Mentalista
    função Math.random(); if. else e else if; operadores lógicos (&& || !)

---
+++ mentalistaid3/README.markdown
@@ -0,0 +1,5 @@
+# Mentalista - ID3
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hrades/pen/OJgzvGm](https://codepen.io/hrades/pen/OJgzvGm).
+
+
---
+++ mentalistaid3/src/script.js
@@ -0,0 +1,35 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado")
+  var chute = parseInt(document.getElementById("valor").value);
+  var tentativas = 0;
+  
+  for(tentativas = 0; tentativas < 4; tentativas++) {
+    if (chute == numeroSecreto) {
+      elementoResultado.innerHTML = "Você acertou!"
+      break
+  } 
+    else if (chute > 10 || chute < 0) {
+      elementoResultado.innerHTML = "NÚMERO INVALIDO\n Você deve digitar números entre 0 e 10"
+      tentativas = tentativas - 1;
+  } 
+    else {
+      if (numeroSecreto < chute){
+        elementoResultado.innerHTML = "Você errou, tente um número menor";
+      } else {
+        elementoResultado.innerHTML = "Você errou, tente um número maior";
+      }
+    }
+  }
+  
+  
+  /*if (tentativas == 3){
+    elementoResultado.innerHTML = "Número de tentativas excedido\n A resposta era " + numeroSecreto;
+    }*/
+  
+  //não consegui adicionar as tentativas do jeito q sei fazer contador, ele mostra direto que o número foi excedido logo após o primeiro chute
+  }
+  
+
